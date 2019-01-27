6fd42fc40a441e43aa6c5dbd1f23df3c3c899c76
akshayblevel <akshayblevel@hotmail.com>
2019-01-27 11:37:04 +0530

    First Commit

---
+++ EFCoreInMemory/ApiContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreInMemory
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ApiContext(DbContextOptions options) : base(options)
+        {
+            LoadCategories();
+        }
+
+        public void LoadCategories()
+        {
+            Category category = new Category() { CategoryName = "Category1" };
+            Categories.Add(category);
+            category = new Category() { CategoryName = "Category2" };
+            Categories.Add(category);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return Categories.Local.ToList<Category>();
+        }
+    }
+}
---
+++ EFCoreInMemory/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EFCoreInMemory.Controllers
+{
+    [Route("api/Category")]
+    public class CategoryController : Controller
+    {
+        private readonly ApiContext apiContext;
+
+        public CategoryController(ApiContext apiContext)
+        {
+            this.apiContext = apiContext;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = apiContext.GetCategories();
+            return Ok(categories);
+        }
+    }
+}
